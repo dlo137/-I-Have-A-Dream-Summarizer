954a93b8000a413e0aca2d6c9e1d64ee262b9620
DAngelo Watson <dangelo.watson1212@gmail.com>
2023-06-02 16:09:59 -0400

    module imports / start of main function

--- speech_summary.py
+++ speech_summary.py
@@ -5,3 +5,13 @@ import bs4
 import nltk
 from nltk.corpus import stopwords
 
+def main():
+    # GRABBING SPEECH FROM URL
+    url = 'http://www.analytictech.com/mb021/mlk.htm'
+    page = requests.get(url)
+    page.raise_for_status()
+
+    #FIND AND JOIN PARAGRAPH TEXTS
+    soup = bs4.BeautifulSoup(page.text, 'html.parser')
+    p_elements = soup.select('p')
+    speech = ''.join(p_elements)
