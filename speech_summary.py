91f4baefd700b975c031856eac521df5ab34412d
DAngelo Watson <dangelo.watson1212@gmail.com>
2023-06-05 15:17:31 -0400

    stop words func & calc freq of words func done

--- speech_summary.py
+++ speech_summary.py
@@ -16,7 +16,7 @@ def main():
     p_elements = soup.select('p')
     speech = ''.join(p_elements)
 
-    # REMOVE PUNCTUATIONS, NUMBERS, & TYPOS
+    # REMOVING PUNCTUATIONS, NUMBERS, & TYPOS
     speech = speech.replace(')mowing', 'knowing')
     speech = re.sub('\s+', '', speech)
     speech_edit = re.sub('[^a-zA-Z','', speech)
@@ -32,8 +32,42 @@ def main():
         else:
             print('\nInput must be in whole numbers\n')
 
-    speech_edit_no_stop = remove_stop_words(speech_edit)
-    word_freq = get_word_freq(speech_edit_no_stop)
-    sent_scores = score_sentences(speech, word_freq, max_words)
 
-    
+    # VARIABLES FOR FUNCTIONS
+    speech_edit_no_stop = remove_stop_words(speech_edit)
+    word_count = get_word_count(speech_edit_no_stop)
+    scores_for_sentences = score_sentences(speech, word_count, max_words)
+
+    # LIST / SUMMARY OF SENTENCE SCORES
+    counts = Counter(scores_for_sentences)
+    summary = counts.most_common(int(number_of_sentences))
+    print('n\SUMMARY:')
+    for i in summary:
+        print(i[0])
+
+    # REMOVE STOP WORDS FUNCTION
+        # return in string
+    def remove_stop_words(speech_edit):
+        stop_words = set(stopwords.words('english'))
+        speech_edit_no_stop = ''
+        for word in nltk.word_tokenize(speech_edit):
+            if word.lower() not in stop_words:
+                speech_edit_no_stop += word + ''
+        return speech_edit_no_stop
+
+    # CALCULATING FREQUENCY OF OCCURENCE OF WORDS
+    def get_word_freq(speech_edit_no_stop):
+        # return a dictionary of word frequency in a string
+        word_freq = nltk.FreqDist(nltk.word_tokenize(speech_edit_no_stop.lower()))
+        return word_freq
+
+    # SCORING SENTENCES
+    def score_sentences(speech, word_freq, max_words):
+        sent_scores
+
+
+
+
+
+
+
