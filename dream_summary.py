b5b636e69aa4e210cf3ef49115f0330bbe918be2
DAngelo Watson <dangelo.watson1212@gmail.com>
2023-06-06 13:48:14 -0400

    stuck on bug, max number per sentences fail

--- dream_summary.py
+++ dream_summary.py
@@ -13,21 +13,21 @@ def main():
 
     # SCAN AND JOIN PARAGRAPH ELEMENTS
     soup = bs4.BeautifulSoup(page.text, 'html.parser')
-    p_elems = soup.select('p')
+    p_elems = [element.text for element in soup.find_all('p')]
     speech = ''.join(p_elems)
 
     # REMOVING PUNCTUATIONS, NUMBERS, & TYPOS
     speech = speech.replace(')mowing', 'knowing')
-    speech = re.sub('\s+', '', speech)
-    speech_edit = re.sub('[^a-zA-Z','', speech)
+    speech = re.sub('\s+', ' ', speech)
+    speech_edit = re.sub('[^a-zA-Z]','', speech)
     speech_edit = re.sub('\s+', '', speech_edit)
 
     # LOGIC FOR USER'S INPUT REQUIRMENTS
     while True:
         max_words = input('Enter max words per sentence for summary: ')
-        number_of_sentences = input('Enter number of sentences for summary')
+        num_sents = input('Enter number of sentences for summary: ')
 
-        if max_words.isdigit() and number_of_sentences.isdigit():
+        if max_words.isdigit() and num_sents.isdigit():
             break
         else:
             print('\nInput must be in whole numbers\n')
@@ -35,49 +35,51 @@ def main():
 
     # VARIABLES FOR FUNCTIONS
     speech_edit_no_stop = remove_stop_words(speech_edit)
-    word_count = get_word_count(speech_edit_no_stop)
-    scores_for_sentences = score_sentences(speech, word_count, max_words)
+    word_freq = get_word_freq(speech_edit_no_stop)
+    sent_scores = score_sentences(speech, word_freq, max_words)
+
 
     # LIST / SUMMARY OF SENTENCE SCORES
-    counts = Counter(scores_for_sentences)
-    summary = counts.most_common(int(number_of_sentences))
-    print('n\SUMMARY:')
+    counts = Counter(sent_scores)
+    summary = counts.most_common(int(num_sents))
+    print('\nSUMMARY:')
     for i in summary:
         print(i[0])
 
-    # REMOVE STOP WORDS FUNCTION
-        # return in string
-    def remove_stop_words(speech_edit):
-        stop_words = set(stopwords.words('english'))
-        speech_edit_no_stop = ''
-        for word in nltk.word_tokenize(speech_edit):
-            if word.lower() not in stop_words:
-                speech_edit_no_stop += word + ''
-        return speech_edit_no_stop
+# REMOVE STOP WORDS FUNCTION
+# return in string
+def remove_stop_words(speech_edit):
+    stop_words = set(stopwords.words('english'))
+    speech_edit_no_stop = ''
+    for word in nltk.word_tokenize(speech_edit):
+        if word.lower() not in stop_words:
+            speech_edit_no_stop += word + ''
+    return speech_edit_no_stop
 
-    # CALCULATING FREQUENCY OF OCCURENCE OF WORDS
-    def get_word_freq(speech_edit_no_stop):
-        # return a dictionary of word frequency in a string
-        word_freq = nltk.FreqDist(nltk.word_tokenize(speech_edit_no_stop.lower()))
-        return word_freq
+# CALCULATING FREQUENCY OF OCCURENCE OF WORDS
+def get_word_freq(speech_edit_no_stop):
+    # return a dictionary of word frequency in a string
+    word_freq = nltk.FreqDist(nltk.word_tokenize(speech_edit_no_stop.lower()))
+    return word_freq
 
-    # SCORING SENTENCES
-    def score_sentences(speech, word_freq, max_words):
-        # return dictionary of sentence scores based on word frequency
-        sent_scores = dict()
-        # tokenizing speech by sentences
-        sentences = nltk.sent_tokenize(speech)
-        # loop sentences to calculate scores, normalize sent by words /sentence, & meet max words restraint
-        for sent in sentences:
-            sent_scores[sent] = 0
-            words = nltk.word_tokenize(sent.lower())
-            sent_word_count = len(words)
-            if sent_word_count <= int(max_words):
-                for word in words:
-                    if word in word_freq():
-                        sent_scores[sent] += word_freq[word]
-                    sent_scores[sent] = sent_scores[sent] / sent_word_count
-                return sent_scores
+# SCORING SENTENCES
+def score_sentences(speech, word_freq, max_words):
+    # return dictionary of sentence scores based on word frequency
+    sent_scores = dict()
+    # tokenizing speech by sentences
+    sentences = nltk.sent_tokenize(speech)
+    # loop sentences to calculate scores, normalize sent by words /sentence, & meet max words restraint
+    for sent in sentences:
+        sent_scores[sent] = 0
+        words = nltk.word_tokenize(sent.lower())
+        sent_word_count = len(words)
+        if sent_word_count <= int(max_words):
+            for word in words:
+                if word in word_freq.keys():
+                    sent_scores[sent] += word_freq[word]
+            sent_scores[sent] = sent_scores[sent] / sent_word_count
+    return sent_scores
+
 
 if __name__ == '__main__':
     main()
