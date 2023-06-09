07f5a25e59b05d62fdc21ae3096a5fd6888fdb6c
DAngelo Watson <dangelo.watson1212@gmail.com>
2023-06-08 23:34:13 -0400

    bug fixed

--- dream_summary.py
+++ dream_summary.py
@@ -5,6 +5,37 @@ import bs4
 import nltk
 from nltk.corpus import stopwords
 
+# REMOVE STOP WORDS FUNCTION
+# return in string
+def remove_stop_words(speech_edit):
+    stop_words = set(stopwords.words('english'))
+    speech_edit_no_stop = ' '
+    for word in nltk.word_tokenize(speech_edit):
+        if word.lower() not in stop_words:
+            speech_edit_no_stop += word + ' '
+    return speech_edit_no_stop
+
+# CALCULATING FREQUENCY OF OCCURENCE OF WORDS
+def get_word_freq(speech_edit_no_stop):
+    # return a dictionary of word frequency in a string
+    word_freq = nltk.FreqDist(nltk.word_tokenize(speech_edit_no_stop.lower()))
+    return word_freq
+
+# SCORING SENTENCES
+def score_sentences(speech, word_freq, max_words):
+    # return dictionary of sentence scores based on word frequency
+    sent_scores = dict()
+    # tokenizing speech by sentences
+    sentences = nltk.sent_tokenize(speech)
+    # loop sentences to calculate scores and enforce max words per sentence
+    for sent in sentences:
+        words = nltk.word_tokenize(sent.lower())
+        if len(words) <= int(max_words):
+            sent_scores[sent] = sum(word_freq[word] for word in words if word in word_freq) / len(words)
+    return sent_scores
+
+
+
 def main():
     # GRABBING SPEECH FROM URL
     url = 'http://www.analytictech.com/mb021/mlk.htm'
@@ -14,13 +45,13 @@ def main():
     # SCAN AND JOIN PARAGRAPH ELEMENTS
     soup = bs4.BeautifulSoup(page.text, 'html.parser')
     p_elems = [element.text for element in soup.find_all('p')]
-    speech = ''.join(p_elems)
+    speech = ' '.join(p_elems)
 
     # REMOVING PUNCTUATIONS, NUMBERS, & TYPOS
     speech = speech.replace(')mowing', 'knowing')
     speech = re.sub('\s+', ' ', speech)
     speech_edit = re.sub('[^a-zA-Z]','', speech)
-    speech_edit = re.sub('\s+', '', speech_edit)
+    speech_edit = re.sub('\s+', ' ', speech_edit)
 
     # LOGIC FOR USER'S INPUT REQUIRMENTS
     while True:
@@ -45,41 +76,6 @@ def main():
     print('\nSUMMARY:')
     for i in summary:
         print(i[0])
-
-# REMOVE STOP WORDS FUNCTION
-# return in string
-def remove_stop_words(speech_edit):
-    stop_words = set(stopwords.words('english'))
-    speech_edit_no_stop = ''
-    for word in nltk.word_tokenize(speech_edit):
-        if word.lower() not in stop_words:
-            speech_edit_no_stop += word + ''
-    return speech_edit_no_stop
-
-# CALCULATING FREQUENCY OF OCCURENCE OF WORDS
-def get_word_freq(speech_edit_no_stop):
-    # return a dictionary of word frequency in a string
-    word_freq = nltk.FreqDist(nltk.word_tokenize(speech_edit_no_stop.lower()))
-    return word_freq
-
-# SCORING SENTENCES
-def score_sentences(speech, word_freq, max_words):
-    # return dictionary of sentence scores based on word frequency
-    sent_scores = dict()
-    # tokenizing speech by sentences
-    sentences = nltk.sent_tokenize(speech)
-    # loop sentences to calculate scores, normalize sent by words /sentence, & meet max words restraint
-    for sent in sentences:
-        sent_scores[sent] = 0
-        words = nltk.word_tokenize(sent.lower())
-        sent_word_count = len(words)
-        if sent_word_count <= int(max_words):
-            for word in words:
-                if word in word_freq.keys():
-                    sent_scores[sent] += word_freq[word]
-            sent_scores[sent] = sent_scores[sent] / sent_word_count
-    return sent_scores
-
 
 if __name__ == '__main__':
     main()
